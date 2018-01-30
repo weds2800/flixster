6ec3eedaac9436e5e0e1256ffdde9e051c0c5e19
weds2800 <terri@thinkquickconsulting.com>
2018-01-30 16:14:40 +0000

    challenge permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end  
 
   private
 
   helper_method :current_lesson
+
+  def require_authorized_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      flash[:alert] ="Invalid Login"
+      redirect_to course_path(current_lesson.section.course)
+    end
+  end   
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end  
